f326301c214a040b54d0077bb1cf873148c0c88a
Theo-SAMINADIN-td <theosaminadin.pro@gmail.com>
2025-12-10 20:54:04 +0100

    update : add the new all-in-one function to pass the selected gathered json datas

--- sonalyse_advisor/agent_backend.py
+++ sonalyse_advisor/agent_backend.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from groq import Groq
 import os
 from config import MODEL
-from json_utils import load_json
+from json_utils import gather_all_extracted_data
 
 load_dotenv()
 
@@ -12,12 +12,12 @@ def read_context_file(file_path: str) -> str:
         return file.read()
 
 
-def interpret_json(json: str, context: str) -> str:
+def interpret_json(json_path: str, context_path: str) -> str:
     """Interpret the given JSON data using a language model with provided context.
 
     Args:
-        json: The JSON data to interpret.
-        context: The context to provide to the model.
+        json_path: The path to the JSON data to interpret.
+        context_path: The path to the context file to provide to the model.
 
     Returns:
         The streaming response from the model.
@@ -28,7 +28,7 @@ def interpret_json(json: str, context: str) -> str:
         messages=[
             {
                 "role": "system",
-                "content": f"System Prompt : {read_context_file(context)}, \n\n JSON Data: \n\n {load_json(json)}",
+                "content": f"System Prompt : {read_context_file(context_path)}, \n\n JSON Data: \n\n {gather_all_extracted_data(json_path)}",
             },
             {
                 "role": "user",
